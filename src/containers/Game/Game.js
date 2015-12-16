f7605d8be89337b969acf7fb696d98500a24e408
Andrew Ray <delvarworld@gmail.com>
2015-12-16 18:21:27 -0500

    ugh

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -4,6 +4,10 @@ import React3 from 'react-three-renderer';
 import THREE from 'three.js';
 import CANNON from 'cannon/src/Cannon';
 
+// see http://stackoverflow.com/questions/24087757/three-js-and-loading-a-cross-domain-image
+THREE.ImageUtils.crossOrigin = '';
+THREE.TextureLoader.crossOrigin = '';
+
 const radius = 20;
 const speed = 0.1;
 const clock = new THREE.Clock();
@@ -12,7 +16,9 @@ const height = 400;
 const width = 400;
 
 const shadowD = 20;
-const boxes = 10;
+const boxes = 50;
+
+const playerRadius = 1.0;
 
 const timeStep = 1 / 60;
 
@@ -20,7 +26,10 @@ const KeyCodes = {
   LEFT: 37,
   RIGHT: 39,
   UP: 38,
-  DOWN: 40
+  DOWN: 40,
+  X: 88,
+  Y: 89,
+  Z: 90
 };
 
 function without( obj, ...keys ) {
@@ -43,7 +52,7 @@ export default class Game extends Component {
 
     this.state = {
       cameraTarget: new THREE.Vector3(0, 0, 0),
-      cameraPosition: new THREE.Vector3(0, 10, 0),
+      cameraPosition: new THREE.Vector3(0, 6, 0),
       cameraQuaternion: new THREE.Quaternion()
           .setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2),
       cubeRotation: new THREE.Euler(),
@@ -68,26 +77,33 @@ export default class Game extends Component {
     const playerBody = new CANNON.Body({ mass });
     this.playerBody = playerBody;
 
-    const playerShape = new CANNON.Sphere( 0.25 );
+    const playerShape = new CANNON.Sphere( playerRadius );
     this.playerShape = playerShape;
 
     playerBody.addShape(playerShape);
     playerBody.position.set( 0, 0, 0 );
     world.addBody(playerBody);
     bodies.push({
+      scale: new THREE.Vector3( 1, 1, 1 ),
       geometry: 'playerGeometry',
       body: playerBody
     });
 
-    const boxShape = new CANNON.Box(new CANNON.Vec3(0.25, 0.25, 0.25));
+    const boxSize = 1.0;
+    const boxShape = new CANNON.Box( new CANNON.Vec3( boxSize / 2, boxSize / 2, boxSize / 2 ) );
 
     for( let i = 0; i < boxes; ++i ) {
       const boxBody = new CANNON.Body({ mass });
 
       boxBody.addShape(boxShape);
-      boxBody.position.set(-2.5 + Math.random() * 5, 2.5 + Math.random() * 5, -2.5 + Math.random() * 5);
+      boxBody.position.set(
+        -2.5 + Math.random() * 5,
+        2.5 + Math.random() * 5,
+        -2.5 + Math.random() * 5
+      );
       world.addBody(boxBody);
       bodies.push({
+        scale: new THREE.Vector3( boxSize, boxSize, boxSize ),
         geometry: 'cubeGeo',
         body: boxBody
       });
@@ -126,6 +142,8 @@ export default class Game extends Component {
 
       window.addEventListener( 'keydown', this.onKeyDown );
       window.addEventListener( 'keyup', this.onKeyUp );
+
+      this.setState({ created: true });
 
     }
 
@@ -169,9 +187,10 @@ export default class Game extends Component {
   }
 
   _getMeshStates() {
-    return this.bodies.map( ( { geometry, body }, bodyIndex) => {
+    return this.bodies.map( ( { scale, geometry, body }, bodyIndex) => {
       const { position, quaternion } = body;
       return {
+        scale,
         geometry,
         position: new THREE.Vector3().copy(position),
         quaternion: new THREE.Quaternion().copy(quaternion)
@@ -183,7 +202,7 @@ export default class Game extends Component {
 
     this.updatePhysics();
 
-    this.setState({
+    const state = {
       meshStates: this._getMeshStates(),
       lightPosition: new THREE.Vector3(
         radius * Math.sin( clock.getElapsedTime() * speed ),
@@ -195,7 +214,23 @@ export default class Game extends Component {
         this.state.cubeRotation.y + 0.01,
         0
       )
-    });
+    };
+
+    let cameraDelta = 0;
+    if( KeyCodes.Z in this.keysDown ) {
+      cameraDelta = -0.1;
+    } else if( KeyCodes.X in this.keysDown ) {
+      cameraDelta = 0.1;
+    }
+    if( cameraDelta ) {
+      state.cameraPosition = new THREE.Vector3(
+        this.state.cameraPosition.x,
+        this.state.cameraPosition.y + cameraDelta,
+        this.state.cameraPosition.z
+      );
+    }
+
+    this.setState( state );
   }
 
   onKeyDown( event ) {
@@ -213,17 +248,18 @@ export default class Game extends Component {
 
   render() {
 
-    if ( typeof window === 'undefined' ) {
+    if ( !this.state.created ) {
       return <div />;
     }
 
     const { meshStates } = this.state;
 
-    const cubeMeshes = meshStates.map( ( { geometry, position, quaternion }, i ) => {
+    const cubeMeshes = meshStates.map( ( { scale, geometry, position, quaternion }, i ) => {
         return <mesh
             key={i}
             position={position}
             quaternion={quaternion}
+            scale={scale}
             castShadow
         >
             <geometryResource
@@ -235,7 +271,7 @@ export default class Game extends Component {
         </mesh>;
     });
 
-    return (<React3
+    return <React3
       mainCamera="camera"
       width={width}
       height={height}
@@ -258,32 +294,44 @@ export default class Game extends Component {
         <resources>
           <sphereGeometry
             resourceId="playerGeometry"
-
-            radius={0.5}
-
-            widthSegments={10}
-            heightSegments={10}
+            radius={ playerRadius }
+            widthSegments={20}
+            heightSegments={20}
           />
           <boxGeometry
             resourceId="cubeGeo"
 
-            width={0.5}
-            height={0.5}
-            depth={0.5}
+            width={1}
+            height={1}
+            depth={1}
 
-            widthSegments={10}
-            heightSegments={10}
+            widthSegments={1}
+            heightSegments={1}
+          />
+          <texture
+            resourceId="ornateBrick"
+            url={ require( './brick-pattern-ornate.png' ) }
+            wrapS={ THREE.RepeatWrapping }
+            wrapT={ THREE.RepeatWrapping }
+            anisotropy={16}
           />
           <meshPhongMaterial
             resourceId="cubeMaterial"
-
             color={0x888888}
-          />
+          >
+              <textureResource
+                resourceId="ornateBrick"
+              />
+          </meshPhongMaterial>
         </resources>
 
+        <ambientLight
+          color={ 0x777777 }
+        />
+
         <directionalLight
-          color={0xffffff}
-          intensity={1.75}
+          color={ 0xffffff }
+          intensity={ 1.0 }
 
           castShadow
 
@@ -318,7 +366,7 @@ export default class Game extends Component {
           />
         </mesh>
       </scene>
-    </React3>);
+    </React3>;
   }
 
 }
