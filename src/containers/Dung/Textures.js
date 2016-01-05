67adbf8da883cbede3fb16df72a95853f7ee0c82
Andrew Ray <delvarworld@gmail.com>
2016-01-04 21:08:03 -0800

    fart

--- src/containers/Dung/Textures.js
+++ src/containers/Dung/Textures.js
@@ -2,7 +2,9 @@ const Textures = {
     ornateWall1: require( '../Game/love-brick-pattern.png' ),
     ornateWall2: require( '../Game/brick-pattern-color2.png' ),
     brownStuco: require( '../Game/brown-stucco.png' ),
-    purpleStucco: require( '../Game/purple-stucco.png' )
+    purpleStucco: require( '../Game/purple-stucco.png' ),
+    brownStuccoBorder: require( '../Game/brown-stucco-border.png' ),
+    purpleStuccoBorder: require( '../Game/purple-stucco-border.png' )
 };
 
 export default Textures;
